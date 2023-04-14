54e268dc587fb6ca3e81f3a423f1e262bad28065
f3itosa <eric_feitosa13@hotmail.com>
2023-04-13 21:47:41 -0300

    Post para conectar Postman

--- src/main/java/Estudos/LinguagensDev/api/LinguagensController.java
+++ src/main/java/Estudos/LinguagensDev/api/LinguagensController.java
@@ -4,7 +4,8 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -30,6 +31,12 @@ public class LinguagensController {
     public List<Linguagem> obterLinguagens(){
         List<Linguagem> linguagens = repositorio.findAll();
         return linguagens;
+    }
+
+    @PostMapping("/linguagens")
+    public Linguagem NovaLinguagem (@RequestBody Linguagem linguagem){
+        Linguagem linguagemSave = repositorio.save(linguagem);
+        return linguagemSave;
     }
     
 }
